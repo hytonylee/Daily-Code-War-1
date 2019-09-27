b0f2896240fa23d515bff1573abda6c456d7abf4
Tony Lee <30307659+hytonylee@users.noreply.github.com>
2019-09-27 15:06:08 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+describe( "stringToNumber", function(){
+  it( "should work for the examples" , function(){
+    Test.assertEquals(stringToNumber("1234"),1234)
+    Test.assertEquals(stringToNumber("605"), 605)
+    Test.assertEquals(stringToNumber("1405"),1405)
+    Test.assertEquals(stringToNumber("-7"),  -7)
+  });
+});
